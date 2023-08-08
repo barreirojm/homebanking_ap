24093e617cca7e4692a1eb2b61ca677b43589b9f
Juan Manuel Barreiro <98756035+barreirojm@users.noreply.github.com>
2023-08-08 18:41:19 -0300

    Task 1 (#1)
    * copiado carpeta static
    
    * static/models/repositories y clase clients
    
    * configuracion application.properties
    
    * configuracion build.gradle
    
    * correcciones varias
    
    * agregados archivos
    
    * ultimos retoques para la entrega
    
    * fix email
    
    * fix id integer to long

--- src/main/java/com/ap/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/homebanking/HomebankingApplication.java
@@ -1,13 +1,30 @@
 package com.ap.homebanking;
 
+import com.ap.homebanking.models.Client;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.ap.homebanking.repositories.ClientRepository;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args -> {
+			Client client1 = new Client("Melba", "Morel", "melba@mindhub.com");
+			Client client2 = new Client("Juan", "Barreiro","jmb@mail.com");
+			Client client3 = new Client("Denise", "Marelli", "denu@mail.com");
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+			clientRepository.save(client3);
+		});
 	}
 
 }
---
+++ src/main/java/com/ap/homebanking/models/Client.java
@@ -0,0 +1,61 @@
+package com.ap.homebanking.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+public class Client {
+
+    // Atributos de la clase Client
+    // @Id indicara cuál será la clave primaria de nuestra clase.
+    // @Entity indica a Spring que nos genere una tabla en la base de datos.
+    // @GeneratedValueThe annotation
+    // may be applied to a primary key property or field of an entity or mapped
+    // superclass in conjunction with the Id annotation.
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Constructor vacio por defecto sirve para SpringBoot (siempre se usa)
+    public Client(){
+    }
+
+    // Constructor full con los parametros
+    public Client(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Métodos accesores getters y setters
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
