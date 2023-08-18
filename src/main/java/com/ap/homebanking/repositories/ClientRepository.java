4fb77f1a24e473772d343a790691766d5d113f9b
barreirojm <barreirojuanmanuel@gmail.com>
2023-08-18 11:51:22 -0300

    agregado paquete configurations y creada la clase WebAuthentication

--- src/main/java/com/ap/homebanking/repositories/ClientRepository.java
+++ src/main/java/com/ap/homebanking/repositories/ClientRepository.java
@@ -8,13 +8,8 @@ import com.ap.homebanking.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-/*
-@RepositoryRestResource que le indica a Spring que debe generar el código necesario
-para que se pueda administrar la data de la aplicación desde el navegador usando JSON,
-es decir se crea una API REST automática que expone los recursos
-de cada repositorio anotado con @RepositoryRestResource.
- */
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client, Long> {
         Client findByEmail (String email);
+
     }
