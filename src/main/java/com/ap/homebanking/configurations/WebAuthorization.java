3e2b6ccd1cdfc95ea27ec9f637d683599265f36d
barreirojm <barreirojuanmanuel@gmail.com>
2023-08-20 17:09:10 -0300

    front actualizado

--- src/main/java/com/ap/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/ap/homebanking/configurations/WebAuthorization.java
@@ -1,6 +1,7 @@
 package com.ap.homebanking.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebAuthorization extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .antMatchers("/**").hasAuthority("USER");
+
+        http.formLogin()
+                .usernameParameter("name")
+                .passwordParameter("pwd")
+                .loginPage("/app/login");
+
+        http.logout().logoutUrl("/app/logout");
+
+    }
 }
