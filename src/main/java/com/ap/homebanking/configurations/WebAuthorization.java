f2b8a981ba4e1994c09d900662161f8b03d88a21
barreirojm <barreirojuanmanuel@gmail.com>
2023-08-20 18:08:23 -0300

    SecurityFilterChain agregado a WebAuthorization

--- src/main/java/com/ap/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/ap/homebanking/configurations/WebAuthorization.java
@@ -1,16 +1,24 @@
 package com.ap.homebanking.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @EnableWebSecurity
 @Configuration
-public class WebAuthorization extends WebSecurityConfigurerAdapter {
+public class WebAuthorization {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
@@ -22,6 +30,32 @@ public class WebAuthorization extends WebSecurityConfigurerAdapter {
                 .loginPage("/app/login");
 
         http.logout().logoutUrl("/app/logout");
+
+        http.csrf().disable();
+
+        http.headers().frameOptions().disable();
+
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+
+        return http.build();
+
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+
+        }
 
     }
 }
