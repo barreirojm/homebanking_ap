ca3792336157a0ac88ef86fcf0a39b4ab40faa5a
barreirojm <barreirojuanmanuel@gmail.com>
2023-08-29 09:32:49 -0300

    TransactionsController agregado

---
+++ src/main/java/com/ap/homebanking/controllers/TransactionController.java
@@ -0,0 +1,32 @@
+package com.ap.homebanking.controllers;
+
+import com.ap.homebanking.repositories.AccountRepository;
+import com.ap.homebanking.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class TransactionController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Transactional
+    @RequestMapping(path = "/transactions", method = RequestMethod.POST)
+    public ResponseBody<Object> transaction (@RequestParam)
+
+
+
+
+
+
+
+
+
+
+}
