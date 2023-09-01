ceaa2ae833ee8646d024e10c199ea7fbc3f959a1
barreirojm <barreirojuanmanuel@gmail.com>
2023-09-01 10:50:53 -0300

    Agregado LoanController y LoanApplicationDTO

--- src/main/java/com/ap/homebanking/controllers/LoanController.java
+++ src/main/java/com/ap/homebanking/controllers/LoanController.java
@@ -1,4 +1,41 @@
 package com.ap.homebanking.controllers;
 
+import com.ap.homebanking.dtos.LoanApplicationDTO;
+import com.ap.homebanking.repositories.AccountRepository;
+import com.ap.homebanking.repositories.LoanRepository;
+import com.ap.homebanking.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class LoanController {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Transactional
+    @PostMapping("/loans")
+    public ResponseEntity<Object> applyForLoan(@RequestBody LoanApplicationDTO loanApplicationDTO,
+                                               Authentication auth) {
+
+
+
+
+
+
+    }
+
 }
