fceec61a6e3d51c445ca754d4b07e9281543db30
barreirojm <barreirojuanmanuel@gmail.com>
2023-09-12 15:56:24 -0300

    Repositories Testing

---
+++ src/test/java/com/ap/homebanking/RepositoriesTest.java
@@ -0,0 +1,36 @@
+package com.ap.homebanking;
+
+
+import com.ap.homebanking.models.Loan;
+import com.ap.homebanking.repositories.LoanRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@SpringBootTest
+public class RepositoriesTest {
+    @Autowired
+    LoanRepository loanRepository;
+
+    @Test
+    public void existLoans(){
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans,is(not(empty())));
+    }
+
+    @Test
+    public void existPersonalLoan(){
+        List<Loan> loans = loanRepository.findAll();
+        assertThat(loans, hasItem(hasProperty("name", is("Mortgage"))));
+    }
+
+}
